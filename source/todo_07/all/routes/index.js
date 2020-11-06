c2b771b80e1b6e398b5727d1ca03b431707206ac
Kenta Iwadate <k.iwadate@atomitech.jp>
2020-11-06 22:34:51 +0900

    todo表示機能作成

--- source/todo_07/all/routes/index.js
+++ source/todo_07/all/routes/index.js
@@ -1,63 +1,54 @@
 const express = require('express');
 const router = express.Router();
 const knex = require("../db/knex");
+let isAuth;
 
-/* GET home page. */
 router.get('/', function (req, res, next) {
-  //if (req.isAuthenticated()) {
-  let isAuth = false;
-  console.debug("セッション情報");
-  console.debug(req.session.username);
-  if (req.session.username) {
+  if (req.session.userid && req.session.username) {
     isAuth = true;
-  }
-  console.debug("isAuth");
-  console.debug(isAuth);
-  const userId = 1;
-  //const userid = req.session.userid;
-  //const username = req.session.username;
-  knex("tasks")
-    .where({id: userId})
-    .select("*")
-    .then((results) => {
-      if (results.length === 0) {
-        throw new Error("User not found");
-      } else {
-        console.debug(...results);
+    console.debug(isAuth);
+    knex("tasks")
+      .where({user_id: req.session.userid})
+      .select("*")
+      .then((results) => {
+        const tasks = JSON.parse(JSON.stringify(results));
         res.render("index", {
-          results: results,
           title: "",
           isAuth: isAuth,
-          //isAuth: req.isAuthenticated(),
+          tasks: tasks,
+        })
+      })
+      .catch(function (err) {
+        console.error(err);
+        res.render("index", {
+          title: "",
+          errorMessage: [content],
+          isAuth: isAuth,
         });
-      }
+      });
+  } else {
+    isAuth = false;
+    res.render("index", {
+      title: "",
+      isAuth: isAuth,
     });
+  }
 });
 
 router.post('/', function (req, res, next) {
-  const userId = 1;
-  const content = req.body.content;
-
-  let isAuth = false;
-  if (req.session.username) {
-    isAuth = true;
-  }
-
   knex("tasks")
-    .insert({user_id: userId, content: content})
+    .insert({user_id: req.session.userid, content: req.body.content})
     .then(function () {
       res.redirect("/");
     })
     .catch(function (err) {
       console.error(err);
-      res.render("/", {
+      res.render("index", {
         title: "",
         errorMessage: [content],
         isAuth: isAuth,
       });
     });
-
-  res.render('index', {title: 'Express'});
 });
 
 module.exports = router;
