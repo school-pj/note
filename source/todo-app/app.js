0a74e9d93922eca6a5ff6be0fa5443ef895a2be9
Kenta Iwadate <k.iwadate@atomitech.jp>
2020-11-06 13:20:06 +0900

    セッションの設定追加

--- source/todo-app/app.js
+++ source/todo-app/app.js
@@ -3,11 +3,25 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var sessionStore = new session.MemoryStore;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var signinRouter = require('./routes/signin');
+var signupRouter = require('./routes/signup');
+var logoutRouter = require('./routes/logout');
 
 var app = express();
+
+app.use(cookieParser('secret'));
+app.use(session({
+  cookie: { maxAge: 60000 },
+  store: sessionStore,
+  saveUninitialized: true,
+  resave: 'true',
+  secret: 'secret'
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +35,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/signin', signinRouter);
+app.use('/signup', signupRouter);
+app.use('/logout', logoutRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
