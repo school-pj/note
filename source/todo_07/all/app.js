cd3cd7c882cd80d6aded959dd36e53bdf73b1784
Kenta Iwadate <k.iwadate@atomitech.jp>
2020-11-06 23:25:57 +0900

    セッション期限を延長

--- source/todo_07/all/app.js
+++ source/todo_07/all/app.js
@@ -1,27 +1,30 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var session = require('express-session');
-var sessionStore = new session.MemoryStore;
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const session = require('express-session');
+const secret = "secretCuisine123";
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var signinRouter = require('./routes/signin');
-var signupRouter = require('./routes/signup');
-var logoutRouter = require('./routes/logout');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const signinRouter = require('./routes/signin');
+const signupRouter = require('./routes/signup');
+const logoutRouter = require('./routes/logout');
 
-var app = express();
+const app = express();
 
 app.use(cookieParser('secret'));
-app.use(session({
-  cookie: { maxAge: 60000 },
-  store: sessionStore,
-  saveUninitialized: true,
-  resave: 'true',
-  secret: 'secret'
-}));
+app.use(
+  session({
+    secret: secret,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 60 * 60 * 1000,
+    },
+  })
+);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -29,7 +32,7 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -40,12 +43,12 @@ app.use('/signup', signupRouter);
 app.use('/logout', logoutRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
