6cd01d9f0ec54f80f3a2e23083e1291b34244e38
Kenta Iwadate <k.iwadate@atomitech.jp>
2020-11-09 13:35:34 +0900

    タスク削除機能作成

--- source/todo_07/all/routes/index.js
+++ source/todo_07/all/routes/index.js
@@ -4,10 +4,12 @@ const knex = require("../db/knex");
 let isAuth;
 
 router.get('/', function (req, res, next) {
-  if (req.session.userid && req.session.username) {
+  const userId = req.session.userid;
+  const userName = req.session.username;
+  if (userId && userName) {
     isAuth = true;
     knex("tasks")
-      .where({user_id: req.session.userid})
+      .where({user_id: userId})
       .select("*")
       .then((results) => {
         const tasks = JSON.parse(JSON.stringify(results));
@@ -16,6 +18,7 @@ router.get('/', function (req, res, next) {
           errorMessage: "",
           isAuth: isAuth,
           tasks: tasks,
+          userId: userId,
         })
       })
       .catch(function (err) {
@@ -37,19 +40,43 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/', function (req, res, next) {
-  knex("tasks")
-    .insert({user_id: req.session.userid, content: req.body.content})
-    .then(function () {
-      res.redirect("/");
-    })
-    .catch(function (err) {
-      console.error(err);
-      res.render("index", {
-        title: "",
-        errorMessage: [err.sqlMessage],
-        isAuth: isAuth,
+  const userId = req.session.userid;
+  const content = req.body.content;
+  if (Object.keys(req.body)[0] === "content") {
+    knex("tasks")
+      .insert({user_id: userId, content: content})
+      .then(function () {
+        res.redirect("/");
+      })
+      .catch(function (err) {
+        console.error(err);
+        res.render("index", {
+          title: "",
+          errorMessage: [err.sqlMessage],
+          isAuth: isAuth,
+        });
       });
-    });
+
+  } else if (Object.keys(req.body)[0] === "taskId") {
+    const taskId = req.body.taskId;
+    knex("tasks")
+      .where({
+        id: taskId,
+        user_id: userId,
+      })
+      .del()
+      .then(function () {
+        res.redirect("/");
+      })
+      .catch(function (err) {
+        console.error(err);
+        res.render("index", {
+          title: "",
+          errorMessage: [err.sqlMessage],
+          isAuth: isAuth,
+        });
+      });
+  }
 });
 
 module.exports = router;
